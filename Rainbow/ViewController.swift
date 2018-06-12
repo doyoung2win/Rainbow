a79d04d42558e17bdfc7043ce2711afec8279ebe
Do Suh <doyoung.suh@gmail.com>
2018-06-11 18:51:18 -0700

    Initial Commit

---
+++ Rainbow/ViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ViewController.swift
+//  Rainbow
+//
+//  Created by Do on 6/11/18.
+//  Copyright © 2018 Do. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+
+    @IBAction func turnBackgroundRed(_ sender: Any) {
+        view.backgroundColor = .red
+    }
+    
+    @IBAction func turnBackgroundOrange(_ sender: Any) {
+        view.backgroundColor = .orange
+    }
+    
+    
+    @IBAction func turnBackgroundYellow(_ sender: Any) {
+        view.backgroundColor = .yellow
+    }
+    
+    
+    @IBAction func turnBackgroundGreen(_ sender: Any) {
+        view.backgroundColor = .green
+    }
+    
+    
+    @IBAction func turnBackgroundBlue(_ sender: Any) {
+        view.backgroundColor = .blue
+    }
+    
+    
+    @IBAction func turnBackgroundIndigo(_ sender: Any) {
+        view.backgroundColor = .magenta
+    }
+    
+
+}
+
